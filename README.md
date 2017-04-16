499b259f2d1191a2d18979877e1ae07d5f495aba
Ravil Garipov <ravildgar@gmail.com>
2017-04-16 18:54:28 +0000

    create boilerplate for my next react apps

---
+++ README.md
@@ -0,0 +1,9 @@
+# react-boilerplate
+A boilerplate that I can use further in react development
+
+# How To Use
+1) Install packages through the node.js using the command "npm install"
+2) Run the server through the node.js using the command "node app.js"
+3) Open localhost:8080
+4) Start webpack through "webpack -w"
+5) Add all cool things that you really want!
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 module.exports = {
-    entry: './public/app.jsx',
+    entry: './client/app.jsx',
     output: {
         path: __dirname,
         filename: './public/bundle.js'
     },
     resolve: {
+        modules: [__dirname, 'node_modules'],
+        alias: {
+            // Здесь прописаны файлы и пути к ним, чтобы можно было в приложении обращаться к ним непосредственно
+            // Greeter: 'public/components/Greeter.jsx',
+            // GreeterMessage: 'public/components/GreeterMessage.jsx',
+            // GreeterForm: 'public/components/GreeterForm.jsx'
+        },
         extensions: ['*', '.js', '.jsx']
     },
-    module:{
-        loaders: [
-                {
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['react', 'es2015']
-                    },
-                    test: /\.jsx?$/,
-                    exclude: /(node_modules|bower_components)/
-                }
-            ]
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            },
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/
+        }]
     }
 };
